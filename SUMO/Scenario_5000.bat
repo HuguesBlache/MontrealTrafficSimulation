2892b33705975b1fa6e8399a3cd98e4e43f5a1c4
HuguesBlache <hugues.blache@polymtl.ca>
2021-04-30 16:09:37 -0400

    Ajout de carte, configuration sumo ect ... avec un programme de 5000 voitures

---
+++ SUMO/Scenario_5000.bat
@@ -0,0 +1,12 @@
+
+
+FOR /L %%s IN (110,20,190) DO (
+  ECHO %%s
+  od2trips --configuration-file .\od2trips\od2trips.config.xml --taz-files .\TAZ\TAZ.taz.xml --od-matrix-files .\Matrice\OD_Montreal_5000.od.txt --seed %%s --output-prefix seed%%s --output-file ./SansChangementdeMatrice/5000/od2trips/od_5000.odtrips.xml
+  duarouter --configuration-file .\duarouter\duarouter_configuration.xml --seed %%s  --net-file .\Carte\net\MontrealJointure.net.xml --additional-files ./Vtype/Vtype.xml,.\TAZ\TAZ.taz.xml --route-files ./SansChangementdeMatrice/5000/od2trips/seed%%sod_5000.odtrips.xml  --output-prefix seed%%s --output-file ./SansChangementdeMatrice/5000/duarouter/od_5000.odtrips.rou.xml
+  sumo.exe --seed %%s -c Simulation.sumocfg --net-file .\Carte\net\MontrealJointure.net.xml --route-files ./SansChangementdeMatrice/5000/duarouter/seed%%sod_5000.odtrips.rou.xml  --output-prefix seed%%s- --summary ./SansChangementdeMatrice/5000/summary/running
+  REM --tripinfo-output tripinfo%%s
+  REM python C:\Users\hugue\Sumo\tools\xml\xml2csv.py seed%%s-running%%s  
+  REM python C:\Users\hugue\Sumo\tools\xml\xml2csv.py seed%%s-tripinfo%%s
+  REM python "C:\Program Files (x86)\Eclipse\Sumo\tools\xml\xml2csv.py" .\seed%%s-lanedata.xml
+)
