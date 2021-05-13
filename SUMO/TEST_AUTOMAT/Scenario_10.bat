7623b233161f91c7db3c6b6c939bb9f7fafd02d2
HuguesBlache <hugues.blache@polymtl.ca>
2021-05-13 16:50:46 -0400

    Ajout scenario avec --timeline

--- SUMO/TEST_AUTOMAT/Scenario_10.bat
+++ SUMO/TEST_AUTOMAT/Scenario_10.bat
@@ -2,7 +2,7 @@
 REM Compilation des cartes et créeation de la carte
 
 wget  -O ".\Carte\CarteMontreal.osm" --post-file=".\Carte\Montreal_Highway.overpass.ql.txt" "https://overpass.nchc.org.tw/api/interpreter"
-netconvert --osm-files ./Carte/CarteMontreal.osm -o ./Carte/MontrealJointure.net.xml 
+netconvert --osm-files ./Carte/CarteMontreal.osm -o ./Carte/MontrealJointure.net.xml  --remove-edges.by-type highway.unclassified,highway.residential
 
 
 REM Compilation des Taz et créeation de Taz
@@ -13,7 +13,7 @@ python .\function\generateTAZBuildingsFromOSM.py --osm ./Taz/Taz.osm --net ./Car
 
 FOR /L %%s IN (110,20,190) DO (
   ECHO %%s
-  od2trips --configuration-file .\od2trips\od2trips.config.xml --taz-files .\TAZ\Taz.xml --od-matrix-files .\Matrice\OD_Montreal_5000.od.txt --seed %%s --output-prefix seed%%s --output-file ./Scenario/5000/od2trips/od_5000.odtrips.xml
+  od2trips --configuration-file .\od2trips\od2trips.config.xml --taz-files .\TAZ\Taz.xml --od-matrix-files .\Matrice\OD_Montreal_5000.od.txt --seed %%s --output-prefix seed%%s --output-file ./Scenario/5000/od2trips/od_5000.odtrips.xml --scale 0.1
   duarouter --configuration-file .\duarouter\duarouter_configuration.xml --seed %%s  --net-file .\Carte\MontrealJointure.net.xml --additional-files ./Vtype/Vtype.xml,.\TAZ\Taz.xml --route-files ./Scenario/5000/od2trips/seed%%sod_5000.odtrips.xml  --output-prefix seed%%s --output-file ./Scenario/5000/duarouter/od_5000.odtrips.rou.xml
   sumo.exe --seed %%s -c Simulation.sumocfg --net-file .\Carte\MontrealJointure.net.xml --route-files ./Scenario/5000/duarouter/seed%%sod_5000.odtrips.rou.xml  --output-prefix seed%%s- --summary ./Scenario/5000/summary/running
   REM --tripinfo-output tripinfo%%s
